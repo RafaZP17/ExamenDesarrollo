c1099f5956f722bcbbf05152d67ac1aef8c67e9c
rafael_zapien <rafael.zapien@uabc.edu.mx>
2026-09-10 18:23:06 -0700

    Examen Desarrollo Base

---
+++ persistencia/src/main/java/mx/desarrollo/persitence/dao/UsuarioDAO.java
@@ -0,0 +1,28 @@
+package mx.desarrollo.persitence.dao;
+
+
+import jakarta.persistence.EntityManager;
+import mx.desarrollo.entity.Usuario;
+import mx.desarrollo.persitence.persistence.AbstractDAO;
+
+import java.util.List;
+
+public class UsuarioDAO extends AbstractDAO<Usuario> {
+    private final EntityManager entityManager;
+
+    public UsuarioDAO(EntityManager em) {
+        super(Usuario.class);
+        this.entityManager = em;
+    }
+
+    public List<Usuario> obtenerTodos(){
+        return entityManager
+                .createQuery("SELECT u FROM Usuario u", Usuario.class)
+                .getResultList();
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+}
