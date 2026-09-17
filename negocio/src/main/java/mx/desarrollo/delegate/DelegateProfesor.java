9c3b045ffa3d9d066779db613a2c6ef559f93bdf
Jorge_Alapizco <alapizco.jorge@uabc.edu.mx>
2026-09-16 18:55:27 -0700

    Capa de Negocio: Creación de DelegateProfesor con reglas de validación para campos de texto y RFC.

---
+++ negocio/src/main/java/mx/desarrollo/delegate/DelegateProfesor.java
@@ -0,0 +1,34 @@
+package mx.desarrollo.delegate;
+
+import mx.desarrollo.entity.Profesor;
+import mx.desarrollo.persitence.dao.ProfesorDAO;
+
+public class DelegateProfesor {
+
+
+    private ProfesorDAO profesorDAO;
+
+    public DelegateProfesor() {
+        this.profesorDAO = new ProfesorDAO();
+    }
+
+    // Este es el método que validará todo antes de guardar
+    public void registrarProfesor(Profesor profesor) throws Exception {
+
+        //  Válida que los campos de texto no superen los 50 caracteres
+        if (profesor.getNombre().length() > 50 ||
+                profesor.getApellidoPaterno().length() > 50 ||
+                profesor.getApellidoMaterno().length() > 50) {
+            throw new Exception("Error: El nombre y los apellidos no pueden exceder los 50 caracteres.");
+        }
+
+        //  Validar el RFC
+        String rfc = profesor.getRfc();
+        if (rfc == null || rfc.trim().isEmpty() || rfc.length() != 13) {
+            throw new Exception("Error: El RFC debe contener exactamente 13 caracteres.");
+        }
+
+        // Si sobrevive a las validaciones de arriba, le damos luz verde al DAO para guardarlo en MySQL
+        profesorDAO.guardar(profesor);
+    }
+}
