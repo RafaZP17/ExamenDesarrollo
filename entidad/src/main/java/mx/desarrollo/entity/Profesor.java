4d6e9fcf6d8b8c1b910bd9534d8559a697ed0550
Jorge_Alapizco <alapizco.jorge@uabc.edu.mx>
2026-09-16 18:15:58 -0700

    Creación de la entidad Profesor con anotaciones de Hibernate

---
+++ entidad/src/main/java/mx/desarrollo/entity/Profesor.java
@@ -0,0 +1,76 @@
+package mx.desarrollo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "profesor")
+public class Profesor {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_profesor")
+    private int idProfesor;
+
+
+    @Column(name = "nombre", length = 50, nullable = false)
+    private String nombre;
+
+
+    @Column(name = "apellido_paterno", length = 50, nullable = false)
+    private String apellidoPaterno;
+
+
+    @Column(name = "apellido_materno", length = 50, nullable = false)
+    private String apellidoMaterno;
+
+
+    @Column(name = "rfc", length = 13, nullable = false)
+    private String rfc;
+
+
+    public Profesor() {
+    }
+
+
+
+    public int getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(int idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+}
